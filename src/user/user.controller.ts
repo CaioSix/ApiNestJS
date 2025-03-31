da562757f7ca971a8400f637b712814a145684eb
Caio Cavalcanti <39663232+CaioSix@users.noreply.github.com>
2025-03-31 02:09:30 -0300

    Instalando algumas dependencias

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,45 +1,57 @@
-import { Body, Controller, Post, Get, Param, Put, Patch, Delete } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Put,
+  Patch,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UpdateUserDTO } from './DTO/update-put-user.dto';
+import { UpdatePathUserDTO } from './DTO/update-patch-user.dto';
 
 @Controller('users')
 export class UserController {
+  @Post()
+  async create(@Body() { email, name, password }: CreateUserDTO) {
+    return { name, email, password };
+  }
 
-    @Post()
-    async create( @Body() body: any){
-        return {body}
-    }
+  @Get()
+  async read() {
+    return { users: [] };
+  }
 
-    @Get()
-    async read(){
-        return {users: []}        
-    }
+  @Get(':id')
+  async readOne(@Param('id', ParseIntPipe) id: number) {
+    return { user: {}, id };
+  }
 
-    @Get(':id')
-    async readOne(@Param() param ){
-        return {user: {}, param}        
-    }
+  @Put(':id')
+  async update(@Body() { email, name, password }: UpdateUserDTO, @Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'put',
+      email, name, password ,
+      id,
+    };
+  }
 
-    @Put(':id')
-    async update(@Body() body, @Param() param){
-        return {
-            method: 'put',
-            body, 
-            param}
-    }
+  @Patch()
+  async updateParcial(@Body() { email, name, password }: UpdatePathUserDTO, @Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'patch',
+      email, name, password ,
+      id,
+    };
+  }
 
-    @Patch()
-    async updateParcial(@Body() body, @Param() param){
-        return {
-            method: 'patch',
-            body, 
-            param}
-    }
-
-    @Delete(':id')
-    async delete(@Param() param){
-        return {
-            param
-        }
-    }
-
-
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return {
+      id,
+    };
+  }
 }
